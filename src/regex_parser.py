b209e31033d340e8e5f43dbec557d9f6510b7cb8
faruk.yld <123uzunince123@gmail.com>
2024-10-09 20:00:33 +0300

    define actions, test for no action case

--- src/regex_parser.py
+++ src/regex_parser.py
@@ -1,44 +1,85 @@
 import pyparsing as pp
+from regex_ast_node import RegexASTNode
 
 
-def repetition_action(tokens_or_ast_node):
-  pass
+no_action = 1
 
 
-def concatenation_action(tokens_or_ast_node):
-  pass
+def repetition_action(tokens_or_ast_nodes):
+  if isinstance(tokens_or_ast_nodes[0],RegexASTNode):
+    ast_node = tokens_or_ast_nodes[0]
+    operator = tokens_or_ast_nodes[1]
+    print("repetition, tokens[0] is instance Node")
+  else:
+    ast_node = tokens_or_ast_nodes[0][0]
+    operator = tokens_or_ast_nodes[0][1]
+    print("repetition, tokens[0] is NOT instance Node")
+  return RegexASTNode.from_repetition(ast_node, operator)
 
 
-def union_action(tokens_or_ast_node):
-  pass
+def binary_op_action(tokens_or_ast_nodes):
+  left = tokens_or_ast_nodes[0][0]
+  op = tokens_or_ast_nodes[0][1]
+  right = tokens_or_ast_nodes[0][2]
+  return RegexASTNode.from_binary(left, right, op)
+
+
+def character_action(tokens_or_ast_nodes):
+  return RegexASTNode.from_literal(tokens_or_ast_nodes[0])
 
 
 character_exp = pp.Word(pp.alphanums, exact=1)
+if not no_action:
+  character_exp.add_parse_action(character_action)
 
 union = "|"
 concatenation = ";"
 repetition = pp.one_of("* + ?")
 
-reg_exp = pp.infix_notation(
-    character_exp,
-    [
-        (repetition, 1, pp.opAssoc.LEFT, repetition_action),
-        (concatenation, 2, pp.opAssoc.LEFT, concatenation_action),
-        (None, 2, pp.opAssoc.LEFT, concatenation_action),
-        (union, 2, pp.opAssoc.LEFT, union_action)
-    ]
-)
+if no_action:
+  reg_exp = pp.infix_notation(
+      character_exp,
+      [
+          (repetition, 1, pp.opAssoc.LEFT),
+          (concatenation, 2, pp.opAssoc.LEFT),
+          (union, 2, pp.opAssoc.LEFT),
+      ]
+  )
+else:
+  reg_exp = pp.infix_notation(
+      character_exp,
+      [
+          (repetition, 1, pp.opAssoc.LEFT, repetition_action),
+          (concatenation, 2, pp.opAssoc.LEFT, binary_op_action),
+          (union, 2, pp.opAssoc.LEFT, binary_op_action),
+      ]
+  )
 
 
 test_cases = [
     "a",
-    "aa",
     "(a)",
+    "a*",
+    "a+",
+    "a?",
+    "a|b;c",
+    "a;b|c",
+    "a+;b+",
+    "a+;b*",
+    "a+;b?",
+    "a*;b*",
+    "a*;b+",
+    "a*;b?",
+    "a?;b?",
+    "a?;b+",
+    "a?;b*",
+
     "a|b",
     "a;b",
     "a;(a|b)*",
     "(a;b|b)*;b+;a?",
 ]
+
 for test_case in test_cases:
   print("test case: ", test_case)
   parse_result = reg_exp.parse_string(test_case)
