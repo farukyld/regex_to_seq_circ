9e2b2a1af19ab8016ad9856a997809cdb30d44b4
farukyld <123uzunince123@gmail.com>
2024-12-23 21:40:45 +0300

    add setup py for python to recgnize the folders as packages

---
+++ src/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="regex_machine",
+    version="0.1",
+    packages=find_packages(),  # Automatically finds packages with __init__.py
+    install_requires=[],
+)
