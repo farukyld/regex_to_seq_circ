c68baba24135cde41f95201b9346f3f85fdd954e
farukyld <123uzunince123@gmail.com>
2025-04-21 06:54:55 +0300

    move setup.py to root directory

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="regex_machine",
     version="0.1",
-    packages=find_packages(),  # Automatically finds packages with __init__.py
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[],
 )
