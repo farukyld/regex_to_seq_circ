0e996783d209b96556ab1e6660ab9c6d5b841bff
farukyld <123uzunince123@gmail.com>
2025-05-01 09:37:06 +0300

    - fix module name (prefix _ the parameter, so that circt ignores the parameter in naming)

--- src/backend/builder.py
+++ src/backend/builder.py
@@ -4,8 +4,6 @@ from pycde.types import Bits
 from pycde.constructs import Reg, Wire,NamedWire
 from pycde.signals import BitsSignal
 from pycde import signals
-from pycde.circt.dialects import comb
-
 
 from backend.circuit_deser import CircuitDeser
 from path_shortcuts import TEST_0_JSON_PATH, generate_sv_output_dir_name
@@ -13,8 +11,8 @@ from color_print import introduce
 
 
 @modparams
-def seq_circt_builder(formal_circuit: CircuitDeser):
-  class SeqCircuit(Module):
+def seq_circt_builder(_formal_circuit: CircuitDeser):
+  class seq_circuit(Module):
     clk = Clock()
     rst = Reset()
     y_o = Output(Bits(1))
@@ -24,21 +22,21 @@ def seq_circt_builder(formal_circuit: CircuitDeser):
     def build(ports):
       # start at zeroth position (to be 1) at reset
       first_position = Bits(1)(1)
-      remaining_positions = Bits(formal_circuit.n_states-1)(0)
+      remaining_positions = Bits(_formal_circuit.n_states-1)(0)
       initial_value = BitsSignal.concat([remaining_positions, first_position])
 
       # Create a register using the Reg construct
-      state = Reg(Bits(formal_circuit.n_states), clk=ports.clk,
+      state = Reg(Bits(_formal_circuit.n_states), clk=ports.clk,
                   rst=ports.rst, rst_value=initial_value)
       state.name = "state"
 
       transition_functions = []
 
       # activate state[0] always if partial_match flag is set
-      transition_functions.append(Bits(1)(not formal_circuit.full_match))
+      transition_functions.append(Bits(1)(not _formal_circuit.full_match))
 
-      formal_circuit.transitions.sort(key=lambda tr: tr[0]) # sort by to_state
-      for transition in formal_circuit.transitions:
+      _formal_circuit.transitions.sort(key=lambda tr: tr[0]) # sort by to_state
+      for transition in _formal_circuit.transitions:
         input_to_consume = transition[1]
         from_states = transition[2]
 
@@ -56,25 +54,26 @@ def seq_circt_builder(formal_circuit: CircuitDeser):
 
       ports.y_o = signals.Or(*[
         transition_functions[accept_state_index]
-        for accept_state_index in formal_circuit.accept_states
+        for accept_state_index in _formal_circuit.accept_states
       ])
 
 
       transition_functions.reverse()
       state.assign(BitsSignal.concat(transition_functions))
-
-  return SeqCircuit
+  return seq_circuit
 
 
 if __name__ == "__main__":
   introduce(__file__)
 
-  # Example usage
-  # Instantiate the parametric register with a bit width of 16
   with open(TEST_0_JSON_PATH, "r") as f:
     json_obj = json.load(f)
   formal_circuit = CircuitDeser.from_dict(json_obj)
   output_dir = generate_sv_output_dir_name()
-  system = System([seq_circt_builder(formal_circuit)], name="seq_circuit",
+
+  top_module = seq_circt_builder(formal_circuit)
+
+  system = System([top_module], name="seq_circuit",
                   output_directory=output_dir)
+
   system.compile()
