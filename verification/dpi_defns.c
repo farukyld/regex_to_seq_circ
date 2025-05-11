0fc1a15d78c239dd24c47a95f4408c05c5d17268
farukyld <123uzunince123@gmail.com>
2025-05-11 09:16:25 +0300

    remove dpi imports, I dind't know systemverilog has standard input output files specified

--- verification/dpi_defns.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "Vtb_seq_circt__Dpi.h"
-int eof_stdin()
-{
-  return feof(stdin);
-}
--- verification/testbench.sv
+++ verification/testbench.sv
@@ -1,18 +1,12 @@
 // see: https://chatgpt.com/share/68136a46-8298-800f-968c-beab5a84ea0f
 module tb_seq_circt;
 
-  import "DPI-C" function int getchar;
-  import "DPI-C" function int putchar(int c);
-  import "DPI-C" function int eof_stdin();
-
   logic clk, rst;
   logic [7:0] x;
   logic y;
 
   // File handles
-  integer input_file, output_file;
-  string input_line;
-  int r;
+  const integer stdin_fd = 32'h8000_0000;
 
   seq_circuit inst (
     .clk(clk),
@@ -22,24 +16,21 @@ module tb_seq_circt;
   );
 
   always #5 clk = ~clk;
-  integer i;
+
   initial begin
     clk = 0;
     rst = 1;
 
     #10 rst = 0;
 
-    i = 0;
-    // while (i < 10) begin
-    while (!(|eof_stdin())) begin
-    i = i +1;
+    while (!$feof(stdin_fd)) begin
     // verilator lint_off WIDTHTRUNC
-      x = getchar();
+      x = $fgetc(stdin_fd);
     // verilator lint_on WIDTHTRUNC
 
       @(negedge clk);
       // verilator lint_off IGNOREDRETURN
-      putchar({31'b0,y}+48);
+      $writeb(y);
       // lint_on
     end
 
