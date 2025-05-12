bc00a3b9fd7121e3119e73a40da8998e71597823
farukyld <123uzunince123@gmail.com>
2025-05-12 09:19:58 +0300

    add warning if json is newer than hw

--- test/util/path_shortcuts.py
+++ test/util/path_shortcuts.py
@@ -1,9 +1,10 @@
 from pathlib import Path
-
+from util.color_print import print_red
 MAX_OUTPUTS = 100
 OUTPUTS_PARENT = Path(__file__).parent.parent.parent / "build"
 OUTPUTS_PARENT.mkdir(exist_ok=True, parents=True)
 _JSON_FILE_NAME = "formal.json"
+_HW_DIR_NAME = "hw"
 
 
 def get_next_output_dir_and_json() -> tuple[Path, Path]:
@@ -34,4 +35,10 @@ def get_any_output_dir_and_json() -> tuple[Path, Path]:
     name = f"output_{i:03d}"
     path = OUTPUTS_PARENT / name
     if path.exists():
-      return path, path / _JSON_FILE_NAME
+      json_path = path / _JSON_FILE_NAME
+      hw_dir_name = (path / _HW_DIR_NAME)
+      if json_path.stat().st_mtime > hw_dir_name.stat().st_mtime:
+        print_red(f"{json_path} is newer than {hw_dir_name}, "
+                  f"this may indicate that {hw_dir_name} "
+                  f"doesn't represent the same circuit as {json_path}")
+      return path, json_path
