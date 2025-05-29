3db9bf14bd3770ff4808c04f043492bcb804ec6a
farukyld <123uzunince123@gmail.com>
2025-05-29 11:51:25 +0300

    create partial and full match circuits for all test cases

--- test/modules/test_flow.py
+++ test/modules/test_flow.py
@@ -29,29 +29,30 @@ for pattern in patterns:
   ast_root = regex_parser.regex_pattern_to_ast(pattern)
   color_print.print_green("parsed")
 
-  color_print.print_cyan(
-      "creating the formal definition of sequential circuit and serializing")
-  circt_dict = ast_to_formal_circuit.circuit_dict(ast_root, random.random() > 0.5)
-  color_print.print_green("created, serialized")
+  for partial_match in [True,False]:
+    color_print.print_cyan(
+        f"creating the formal definition of sequential circuit with partial match: {partial_match} and serializing")
+    circt_dict = ast_to_formal_circuit.circuit_dict(ast_root, partial_match)
+    color_print.print_green("created, serialized")
 
-  output_dir, json_path = path_shortcuts.get_next_unused_output_dir_and_json()
-  output_dir.mkdir()
-  color_print.print_cyan(
-      f"dumping formal definition into {json_path}")
-  with open(json_path, "w") as f:
-    f.write(json.dumps(circt_dict, indent=2))
-  color_print.print_green("dumped")
+    output_dir, json_path = path_shortcuts.get_next_unused_output_dir_and_json()
+    output_dir.mkdir()
+    color_print.print_cyan(
+        f"dumping formal definition into {json_path}")
+    with open(json_path, "w") as f:
+        f.write(json.dumps(circt_dict, indent=2))
+    color_print.print_green("dumped")
 
-  color_print.print_cyan(
-      "deserializing json string representing formal definition")
-  deserialized = circuit_deser.CircuitDeser.from_dict(circt_dict)
-  color_print.print_green("deserialized")
+    color_print.print_cyan(
+        "deserializing json string representing formal definition")
+    deserialized = circuit_deser.CircuitDeser.from_dict(circt_dict)
+    color_print.print_green("deserialized")
 
-  color_print.print_cyan(
-      f"building system verilog output under {output_dir}")
-  system = pycde.System([builder.seq_circt_builder(deserialized)], name="seq_circuit",
-                        output_directory=output_dir)
-  system.compile()
-  color_print.print_green("built")
-  color_print.print_green("----\n"
-                          "done")
+    color_print.print_cyan(
+        f"building system verilog output under {output_dir}")
+    system = pycde.System([builder.seq_circt_builder(deserialized)], name="seq_circuit",
+                            output_directory=output_dir)
+    system.compile()
+    color_print.print_green("built")
+    color_print.print_green("----\n"
+                            "done")
